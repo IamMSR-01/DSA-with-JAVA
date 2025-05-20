ceb25b90d689816025ee7fff276d8784ed4fbd83
Mohd Shaqib Raza <iammsr19413m@gmail.com>
2025-05-21 01:36:14 +0530

    Add FirstAndLastOcc class to find first and last occurrence of a character in a string

---
+++ Day1/FirstAndLastOcc.java
@@ -0,0 +1,30 @@
+public class FirstAndLastOcc {
+    public static int first = -1;
+    public static int last = -1;
+
+    public static void firstAndLastOcc(String str, int idx, char element){
+
+        if(idx == str.length()){
+            System.out.println(first);
+            System.out.println(last);
+            return;
+        }
+        char currChar = str.charAt(idx);
+        if(currChar == element){
+            if (first == -1) {
+                first = idx;
+            }else{
+                last = idx;
+            }
+        }
+        if (last == -1) {
+            last = first;
+        }
+        firstAndLastOcc(str, idx + 1, element);
+    }
+
+    public static void main(String[] args){
+        String str = "MohdShaqibRaza";
+        firstAndLastOcc(str, 0, 'a');
+    }
+}
