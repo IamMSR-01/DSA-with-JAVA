f30b16f4b8be5b58e8340e92c3bfea78114d6c1d
Mohd Shaqib Raza <iammsr19413m@gmail.com>
2025-05-30 17:06:04 +0530

    Add longestWord method to TrieDS class for finding the longest word in the Trie

--- Day8/TrieDS.java
+++ Day8/TrieDS.java
@@ -92,13 +92,33 @@ public class TrieDS {
 
         return count + 1;
     }
-    public static void main(String[] args) {
-        String str = "ababa";
 
-        for (int i = 0; i < str.length(); i++) {
-            String suffix = str.substring(i);
-            insert(suffix);
+    public static String ans = "";
+
+    public static void longestWord(Node root, StringBuilder temp){
+        if (root == null) {
+            return;
         }
-        System.out.println(countNode(root));
+        for (int i = 0; i < 26; i++) {
+            if (root.children[i] != null && root.children[i].endOfWord == true) {
+                temp.append((char)(i + 'a'));
+                if (temp.length() > ans.length()) {
+                    ans = temp.toString();
+                }
+                longestWord(root.children[i], temp);
+
+                temp.deleteCharAt(temp.length() - 1);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        String words[] = {"a", "banana", "app", "appl", "ap", "apply", "apple"};
+
+        for (int i = 0; i < words.length; i++) {
+            insert(words[i]);
+        }
+
+        longestWord(root, new StringBuilder(""));
+        System.out.println(ans);
     }
 }
